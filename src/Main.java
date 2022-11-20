851eccb3ed965624bc8099e69984d84390d39ac6
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-20 17:52:42 +0500

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,24 @@ public class Main {
 
         if (studentAge >= 24)
             System.out.println("Человек окончил университет и ему пора искать первую работу");
+
+        // Разделитель
+        System.out.println();
+
+        // Задача 3
+        int totalPlaces = 102;
+        int seatPlaces = 60;
+        int standPlaces = totalPlaces - seatPlaces;
+
+        int passengerCountId = 101;
+
+        if (passengerCountId <= seatPlaces)
+            System.out.println("В вагоне осталось " + (seatPlaces - passengerCountId) + " сидячих и " + standPlaces + " стоячих мест");
+
+        if (passengerCountId > seatPlaces && passengerCountId < totalPlaces)
+            System.out.println("В вагоне осталось 0 сидячих и " + (totalPlaces - passengerCountId) + " стоячих мест");
+
+        if (passengerCountId >= totalPlaces)
+            System.out.println("К сожалению мест не осталось");
     }
 }
