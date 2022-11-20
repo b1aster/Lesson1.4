2913d2bb6c2b45ff763fda9c6f9dfb0995b72fd2
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-20 17:41:06 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,20 @@ public class Main {
         if (age < 18)
             System.out.println("Возраст совершеннолетия ещё не наступил.");
 
+
+        // Разделитель
+        System.out.println();
+
+        // Задача 2
+        int studentAge = 19;
+
+        if (studentAge >= 7 && studentAge < 18)
+            System.out.println("Ребенок ходит в школу");
+
+        if (studentAge >= 18 && studentAge < 24)
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+
+        if (studentAge >= 24)
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
     }
 }
