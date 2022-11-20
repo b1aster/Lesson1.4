c85a2c95bf247291885d0febc43573aa9660bb8b
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-20 17:34:11 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        int age = 18;
+
+        if (age >= 18)
+            System.out.println("Поздравляю с совершеннолетием!");
+
+        if (age < 18)
+            System.out.println("Возраст совершеннолетия ещё не наступил.");
+
     }
 }
